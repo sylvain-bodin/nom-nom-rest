675e8d5832505f1ee33dee5d1c76b1270fb47729
Sylvain Bodin <ky.schweitzer@gmail.com>
2020-05-26 22:32:43 +0200

    Branchement des API avec la base

--- src/routes/recipe.ts
+++ src/routes/recipe.ts
@@ -1,7 +1,20 @@
 import { Router } from 'express';
+import Recipe from '../models/recipe';
 
 const recipeRouter = Router();
 
-recipeRouter.get('/', (req, res) => res.send(''));
+recipeRouter.get('/', async (req, res) => {
+  const recipes = await Recipe.find();
+  return res.send(recipes);
+});
 
+recipeRouter.get('/:id', async (req, res) => {
+  const recipe = await Recipe.findById(req.params.id);
+  return res.send(recipe);
+});
+
+recipeRouter.post('/', async (req, res) => {
+  const recipe = await Recipe.create(req.body);
+  return res.send(recipe);
+});
 export default recipeRouter;
