fd01e3986379b2e36c5109be41b5258bbacadcdf
Sylvain Bodin <ky.schweitzer@gmail.com>
2020-05-30 17:03:58 +0200

    Strict TS

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,10 @@
 import { connect } from 'mongoose';
 import Recipe from '../models/recipe';
 
-const connectDb = () => connect(process.env.DATABASE_URL,
-  { useNewUrlParser: true, useUnifiedTopology: true });
-
+const connectDb = () => connect(process.env.DATABASE_URL as
+    string,
+{ useNewUrlParser: true, useUnifiedTopology: true }, () => {
+});
 const models = { Recipe };
 
 export { connectDb };
