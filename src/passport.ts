74713155df499e9546f11b75a644f8c4216f054c
Sylvain Bodin <ky.schweitzer@gmail.com>
2020-06-14 22:35:28 +0200

    Authentification via Twitter et Google

--- src/passport.ts
+++ src/passport.ts
@@ -1,18 +1,25 @@
 // Configuration des connexions via Passport
-import passport from 'passport';
-import { Strategy } from 'passport-facebook';
+import passport, { Profile } from 'passport';
+import { Strategy as FacebookStrategy } from 'passport-facebook';
+import { Strategy as TwitterStrategy } from 'passport-twitter';
+import { Strategy as GoogleStrategy } from 'passport-google-oauth2';
 import User from './models/users';
 
-const { FB_APP_ID = '', FB_APP_SECRET = '', BASE_CALLBACK = '' } = process.env;
-
-passport.use(new Strategy({
-  clientID: FB_APP_ID,
-  clientSecret: FB_APP_SECRET,
-  callbackURL: `${BASE_CALLBACK}/auth/facebook/callback`,
-  profileFields: ['id', 'displayName', 'email'],
+const {
+  FB_APP_ID = '',
+  FB_APP_SECRET = '',
+  BASE_CALLBACK = '',
+  TWITTER_CUSTOMER_KEY = '',
+  TWITTER_CUSTOMER_SECRET = '',
+  GOOGLE_CLIENT_ID = '',
+  GOOGLE_CLIENT_SECRET = '',
+} = process.env;
 
 // eslint-disable-next-line consistent-return
-}, (accessToken, refreshToken, profile, done) => {
+function verify(token: string,
+  tokenSecret: string,
+  profile: Profile,
+  done: (error: any, user?: any, info?: any) => void) {
   if (profile.emails && profile.emails.length > 0) {
     const searchQuery = {
       email: profile.emails[0].value,
@@ -32,8 +39,30 @@ passport.use(new Strategy({
   } else {
     return done('No email');
   }
-}));
+}
 
+passport.use(new FacebookStrategy({
+  clientID: FB_APP_ID,
+  clientSecret: FB_APP_SECRET,
+  callbackURL: `${BASE_CALLBACK}/auth/facebook/callback`,
+  profileFields: ['id', 'displayName', 'email'],
+}, verify));
+
+passport.use(new TwitterStrategy({
+  consumerKey: TWITTER_CUSTOMER_KEY,
+  consumerSecret: TWITTER_CUSTOMER_SECRET,
+  callbackURL: `${BASE_CALLBACK}/auth/twitter/callback`,
+  includeEmail: true,
+  includeEntities: false,
+  includeStatus: false,
+}, verify));
+
+passport.use(new GoogleStrategy({
+  clientID: GOOGLE_CLIENT_ID,
+  clientSecret: GOOGLE_CLIENT_SECRET,
+  callbackURL: `${BASE_CALLBACK}/auth/google/callback`,
+  scope: ['openid', 'profile', 'email'],
+}, verify));
 passport.serializeUser((user, next) => {
   next(null, user);
 });
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -4,12 +4,27 @@ import passport from '../passport';
 const authRouter = Router();
 const baseApp = process.env.BASE_APP || '';
 
-authRouter.get('/facebook',
-  passport.authenticate('facebook'));
+authRouter.get('/facebook', passport.authenticate('facebook'));
+authRouter.get('/facebook/callback', passport.authenticate('facebook', {
+  successRedirect: `${baseApp}/`,
+  failureRedirect: `${baseApp}/login`,
+}));
 
-authRouter.get('/facebook/callback', passport.authenticate('facebook', { failureRedirect: `${baseApp}/login` }), (req, res) => {
-  res.redirect(`${baseApp}/`);
-});
+authRouter.get('/twitter',
+  passport.authenticate('twitter'));
+
+authRouter.get('/twitter/callback', passport.authenticate('twitter', {
+  successRedirect: `${baseApp}/`,
+  failureRedirect: `${baseApp}/login`,
+}));
+
+authRouter.get('/google',
+  passport.authenticate('google'));
+
+authRouter.get('/google/callback', passport.authenticate('google', {
+  successRedirect: `${baseApp}/`,
+  failureRedirect: `${baseApp}/login`,
+}));
 
 authRouter.get('/me', (req, res) => {
   res.send(req.user);
