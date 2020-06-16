e44c9dcb6a38e8b1cdf5d430bee57eb486c56dbd
Sylvain Bodin <ky.schweitzer@gmail.com>
2020-06-16 22:50:51 +0200

    Sauvegarde de la session sur Mongo

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import session from 'express-session';
+import connectMongo from 'connect-mongo';
+import { connection } from 'mongoose';
 import recipeRouter from './routes/recipe';
 import connectDb from './models';
 import { expressLogger, logger } from './services/log';
@@ -11,14 +13,17 @@ import passport from './passport';
 const PORT = process.env.PORT || 3000;
 const BASE_APP = process.env.BASE_APP || '';
 const app = express();
-
+const MongoStore = connectMongo(session);
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: false }));
 app.use(cors({ credentials: true, origin: BASE_APP }));
 app.use(expressLogger);
 app.use(session({
-  secret: 'nom-nom-secret', resave: false, saveUninitialized: false,
+  secret: 'nom-nom-secret',
+  resave: false,
+  saveUninitialized: false,
+  store: new MongoStore({ mongooseConnection: connection }),
 }));
 
 
