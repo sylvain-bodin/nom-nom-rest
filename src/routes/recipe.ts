d4c633828486e896e227e0080639ead061b19773
Sylvain Bodin <ky.schweitzer@gmail.com>
2020-06-28 00:14:44 +0200

    Ajout de la suppression de recette

--- src/routes/recipe.ts
+++ src/routes/recipe.ts
@@ -1,9 +1,11 @@
-import {NextFunction, Request, Response, Router} from 'express';
+import {
+  NextFunction, Request, Response, Router,
+} from 'express';
 import Recipe from '../models/recipe';
-import {logger} from '../services/log';
+import { logger } from '../services/log';
 import importFactory from '../services/import.factory';
 import ImportUtils from '../services/import-utils';
-import {Ingredient} from '../types/ingredient';
+import { Ingredient } from '../types/ingredient';
 
 const recipeRouter = Router();
 
@@ -26,6 +28,19 @@ recipeRouter.get('/', authentifiedHandler, async (req, res) => {
   return res.send(recipes);
 });
 
+recipeRouter.post('/', authentifiedHandler, async (req, res) => {
+  const data = req.body;
+  // @ts-ignore
+  // eslint-disable-next-line no-underscore-dangle
+  data.userId = req.user._id;
+  await Recipe.create(data).catch((err) => {
+    logger.error(err, 'Error getting recipes');
+  });
+  // @ts-ignore
+  // eslint-disable-next-line no-underscore-dangle
+  return res.sendStatus(201);
+});
+
 recipeRouter.get('/:id', authentifiedHandler, async (req, res) => {
   const recipe = await Recipe.findById(req.params.id).catch((err) => {
     logger.error(err, `Error getting recipe ${req.params.id}`);
@@ -33,35 +48,32 @@ recipeRouter.get('/:id', authentifiedHandler, async (req, res) => {
   return res.send(recipe);
 });
 
+recipeRouter.delete('/:id', authentifiedHandler, async (req, res) => {
+  await Recipe.deleteOne({ _id: req.params.id }).catch((err) => {
+    logger.error(err, `Error getting recipe ${req.params.id}`);
+  });
+  return res.sendStatus(204);
+});
+
 recipeRouter.post('/import', authentifiedHandler, async (req, res) => {
-  const {url} = req.body;
+  const { url } = req.body;
   let recipe;
   try {
     recipe = await importFactory.getService(url).import(url);
   } catch (e) {
     logger.error(e);
-    recipe = {name: '', url};
+    recipe = { name: '', url };
   }
   res.send(recipe);
 });
 
-recipeRouter.post('/', authentifiedHandler, async (req, res) => {
-  const data = req.body;
-  // @ts-ignore
-  // eslint-disable-next-line no-underscore-dangle
-  data.userId = req.user._id;
-  const recipe = await Recipe.create(data).catch((err) => {
-    logger.error(err, 'Error getting recipes');
-  });
-  return res.send(recipe);
-});
-
 recipeRouter.post('/ingredients/transform', authentifiedHandler, async (req, res) => {
-  const {text} = req.body;
+  const { text } = req.body;
   const ingredients: Ingredient[] = [];
   text.split('\n').forEach((ingredientText: string) => {
     ingredients.push(ImportUtils.parseIngredient(ingredientText));
   });
   res.send(ingredients);
 });
+
 export default recipeRouter;
